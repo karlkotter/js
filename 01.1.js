bbf26b5dac69302a967042c38b572740f633e953
McKotter <93869121+mckotter@users.noreply.github.com>
2023-10-31 15:08:06 +0200

    Add files via upload

---
+++ 01.1.js
@@ -0,0 +1,18 @@
+/*
+Karl Eerik Kotter
+31.10
+harjutus 1
+*/
+
+
+console.log("Harjutus 1")
+let Number = 4.2;
+let String = "Will code 4 food";
+let Number2 = 123;
+let String2 = "123";
+let Boolean = true;
+let Array  = [1,2,3];
+let Boolean2 = false;
+let String3 = "true";
+let undef;
+console.log(Number,String,Number2, String2, Boolean, Array, Boolean2, String3, undefined);
---
+++ 02.js
@@ -0,0 +1,32 @@
+/*
+Karl Eerik Kotter
+31.10
+harjutus 2
+*/
+
+console.log("Harjutus2")
+
+//Kellaaeg
+let tunnid = 3;
+let minutid = 23;
+let sekundid = 18;
+console.log(tunnid +":"+ minutid +":"+ sekundid +"PM");
+
+//Tsitaat lause sees
+let tsitaat = "If you live a life of make-believe, your life isn't worth anything until you do something that does challenge your reality.";
+console.log(tsitaat +"- Morgan Freeman");
+
+//Mallide kasutamine
+let eesnimi = "Karl";
+let perenimi = "Kotter";
+
+
+let eesnimiMark = eesnimi[0];
+let perenimiMark = perenimi[0];
+tahed = eesnimiMark +"."+ perenimiMark;
+
+console.log(eesnimi, perenimi +" nimetähed on "+ tahed +".");
+
+//Perenime pikkus
+let pereJaEes = "Karl, Kotter";
+  
